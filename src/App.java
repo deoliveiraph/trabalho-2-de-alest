af873ab575866ed4e21a6ded88eee1a53a1f443d
leonardorodriguezherediapucrsonline2 <leonardorodriguezherediapucrsonline2@gmail.com>
2025-06-11 21:43:49 -0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) {
+        App app = new App();
+        app.rodarAplicacao();
+    }
+    public void rodarAplicacao() {
+        //aqui deve ser feito o trabalho
+
+    }
+}
